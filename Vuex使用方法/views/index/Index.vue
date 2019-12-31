7aff7e4cdb2819bb78e4c5133346d637eab072d6
王思垚 <49677680+Wangsiyao-tutu@users.noreply.github.com>
2019-12-31 11:08:54 +0800

    Vuex的使用方法
    Veux的使用方法(简单版本)，没有涉及到mapGetter等

---
+++ Vuex使用方法/views/index/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Content style="padding:20px;">   
+            <Card>
+                 
+            </Card>
+        </Content>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+    },
+    methods:{
+        showModel(){
+            this.$changeProfileDrawer.show();
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+    h1{
+        color:red;
+    }
+</style>
